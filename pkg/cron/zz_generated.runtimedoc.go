323acbd4b9ef9774568e69e110244b338f9ee1da
Morlay <morlay.null@gmail.com>
2025-02-15 12:10:30 +0800

    fix: agent api updates

--- pkg/cron/zz_generated.runtimedoc.go
+++ pkg/cron/zz_generated.runtimedoc.go
@@ -24,46 +24,6 @@ func runtimeDoc(v any, prefix string, names ...string) ([]string, bool) {
 	return nil, false
 }
 
-func (v *IntervalSchedule) RuntimeDoc(names ...string) ([]string, bool) {
-	if len(names) > 0 {
-		switch names[0] {
-		case "Interval":
-			return []string{}, true
-		}
-
-		return nil, false
-	}
-	return []string{}, true
-}
-
-func (v *Job) RuntimeDoc(names ...string) ([]string, bool) {
-	if len(names) > 0 {
-		switch names[0] {
-		case "Cron":
-			return []string{
-				"cron job 配置",
-				"支持 标准格式",
-				"也支持 @every {duration} 等语义化格式",
-			}, true
-		}
-
-		return nil, false
-	}
-	return []string{}, true
-}
-
-func (v *Schedule) RuntimeDoc(names ...string) ([]string, bool) {
-	if len(names) > 0 {
-		switch names[0] {
-		case "Cron":
-			return []string{
-				"cron job 配置",
-				"支持 标准格式",
-				"也支持 @every {duration} 等语义化格式",
-			}, true
-		}
-
-		return nil, false
-	}
+func (*Spec) RuntimeDoc(names ...string) ([]string, bool) {
 	return []string{}, true
 }
